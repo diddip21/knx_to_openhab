ed87227afa8ef9c1f57f4b7c4ec27374f7ff0f29
Patrick G <38922528+diddip21@users.noreply.github.com>
2026-01-27 22:06:48 +0100

    Phase 5: Create web_ui package __init__.py

--- src/knx_to_openhab/web_ui/__init__.py
+++ src/knx_to_openhab/web_ui/__init__.py
@@ -1,7 +1,1 @@
-"""Web UI package for KNX to OpenHAB.
-
-This package contains the Flask web application for converting KNX projects
-through a web interface.
-"""
-
-__all__ = []
+"""Web UI package for KNX to OpenHAB converter."""
